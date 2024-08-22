1aedc23c25063072c80e9067f54105e83cedd16e
Albin Hasanaj Blom <albinhasanaj.blom@learnet.se>
2024-08-22 09:57:55 +0200

    added wave transition

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import { Fragment } from "react";
+import Wave from 'react-wavify'
 
 export default function Home() {
   return (
@@ -9,11 +10,21 @@ export default function Home() {
       <header className="w-full flex justify-center absolute px-4">
         <Navbar />
       </header>
-      <main className="flex flex-col">
-        <div className="min-h-screen flex justify-center blue__gradient">
-          <div className="max-w-[1440px] w-full">
+      <main className="flex flex-col overflow-hidden">
+        <div className="min-h-screen flex justify-center blue__gradient relative">
+          <div className="max-w-[1440px] w-full self-center">
             <Hero />
           </div>
+          <Wave fill='#fff'
+            paused={false}
+            style={{ display: 'flex', width: '100%', position: 'absolute', bottom: 0 }}
+            options={{
+              height: 20,
+              amplitude: 30,
+              speed: 0.15,
+              points: 5
+            }}
+          />
         </div>
       </main>
     </Fragment>
