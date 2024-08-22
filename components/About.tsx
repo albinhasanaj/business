ede372216b9de9b1550bbb177323d9541ebb5ef2
albinhasanaj <albinhasanaj06@hotmail.com>
2024-08-22 10:19:18 +0200

    name test commit

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
             <h2 className='heading2 text-black'>About</h2>
             <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos commodi facere eos nam, sunt, ea illum, dignissimos aut corrupti tempore quasi odio quae obcaecati deleniti amet natus alias voluptatum aspernatur?
                 Deserunt dicta, perferendis corporis doloribus facere, dolorem at asperiores quisquam repellat voluptates deleniti voluptas mollitia inventore earum fugiat beatae, similique nam. Explicabo ipsum quis repellendus molestias necessitatibus omnis totam perferendis.
-                Id cumque, voluptatibus quidem dolorem officia inventore accusantium autem vitae voluptate ea atque provident ratione porro maxime dicta quia consequatur odio neque obcaecati quas corrupti reprehenderit doloremque. Tenetur, inventore corrupti!</p>
+                Id cumque, voluptatibus quidem dolorem officia inventore accusantium autem vitae voluptate ea atque provident ratione porro maxime dicta quia consequatur odio neque obcaecati quas corrupti reprehenderit doloremque. Tenetur, inventore corrupti</p>
         </section>
     )
 }
