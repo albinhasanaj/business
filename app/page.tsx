cdf8c53574e85c5985961544712fbbedc8f72505
Albin Hasanaj Blom <albinhasanaj.blom@learnet.se>
2024-08-22 10:15:40 +0200

    implement about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
@@ -12,12 +13,29 @@ export default function Home() {
       </header>
       <main className="flex flex-col overflow-hidden">
         <div className="min-h-screen flex justify-center blue__gradient relative">
-          <div className="max-w-[1440px] w-full self-center">
+          <div className="max-w-[1440px] w-full">
             <Hero />
           </div>
           <Wave fill='#fff'
             paused={false}
             style={{ display: 'flex', width: '100%', position: 'absolute', bottom: 0 }}
+            options={{
+              height: 20,
+              amplitude: 30,
+              speed: 0.15,
+              points: 5
+            }}
+          />
+        </div>
+        <div className="bg-white w-full flex flex-col items-center">
+          <div className="max-w-[1440px] flex justify-center">
+            <About />
+          </div>
+        </div>
+        <div className="w-full rotate-180">
+          <Wave fill='#fff'
+            paused={false}
+            style={{ display: 'flex', width: '100%' }}
             options={{
               height: 20,
               amplitude: 30,
---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='flex flex-col items-center gap-6 py-12'>
+            <h2 className='heading2 text-black'>About</h2>
+            <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos commodi facere eos nam, sunt, ea illum, dignissimos aut corrupti tempore quasi odio quae obcaecati deleniti amet natus alias voluptatum aspernatur?
+                Deserunt dicta, perferendis corporis doloribus facere, dolorem at asperiores quisquam repellat voluptates deleniti voluptas mollitia inventore earum fugiat beatae, similique nam. Explicabo ipsum quis repellendus molestias necessitatibus omnis totam perferendis.
+                Id cumque, voluptatibus quidem dolorem officia inventore accusantium autem vitae voluptate ea atque provident ratione porro maxime dicta quia consequatur odio neque obcaecati quas corrupti reprehenderit doloremque. Tenetur, inventore corrupti!</p>
+        </section>
+    )
+}
+
+export default About
