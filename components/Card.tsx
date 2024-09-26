6a01187a09cefc442825cfa25ba8ea6671281392
ProgrammerFromSweden <albinhasanaj06@hotmail.com>
2024-09-26 12:26:32 +0200

    chore: tweaks

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,28 +3,27 @@ import React from 'react';
 
 const Card = ({ title, tags }: { title: string; tags: string[] }) => {
     return (
-        <div className="relative card-container w-[300px] h-[300px] md2:w-[350px] md2:h-[350px] bg-gradient-to-tl from-[#3d66f5] to-[#4c19ae] rounded-md p-4">
+        <div className="relative card-container w-full max-w-[250px] sm:max-w-[350px] md:max-w-[400px] lg:max-w-[450px] h-auto bg-gradient-to-tl from-[#3d66f5] to-[#4c19ae] rounded-md p-4">
             {/* Card content */}
             <div className="flex flex-col gap-3 w-full h-full items-start justify-start">
-                <div className="relative w-[100px] h-[100px]">
+                <div className="relative w-full h-[150px] sm:h-[200px] md:h-[250px]">
                     <Image
                         src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
                         alt="image"
-                        width={100}
-                        height={100}
+                        layout="fill"
                         className="rounded-md object-cover"
                     />
                 </div>
 
-                <span className="md2:text-2xl text-xl font-semibold text-white">
+                <span className="text-lg sm:text-xl md:text-2xl font-semibold text-white">
                     {title}
                 </span>
 
-                <div className="flex flex-wrap md2:w-2/3">
+                <div className="flex flex-wrap w-full">
                     {tags.map((tag, index) => (
                         <span
                             key={index}
-                            className="from-[#3d66f5] to-[#4c19ae] bg-gradient-to-tl text-white text-sm px-2 py-1 rounded-md m-1"
+                            className="bg-gradient-to-tl from-[#3d66f5] to-[#4c19ae] text-white text-xs sm:text-sm md:text-base px-2 py-1 rounded-md m-1"
                         >
                             {tag}
                         </span>
--- components/CardsContainer.tsx
+++ components/CardsContainer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Card from './Card';
 import { Services } from '@/constants/services';
+import Button from './Button';
 
 const CardsContainer = () => {
     return (
-        <div className='flex flex-wrap md:justify-end gap-4 justify-center mr-5'>
-            {Services.map((service, index) => (
-                <Card key={index} title={service.title} tags={service.tags} />
-            ))}
+        <div className='flex flex-col'>
+            <div className='flex flex-wrap md:justify-end gap-4 justify-center mr-5'>
+                {Services.map((service, index) => (
+                    <Card key={index} title={service.title} tags={service.tags} />
+                ))}
+            </div>
+            <Button text="Find your goal" classes='self-center bg-gradient-to-tl from-[#3d66f5] to-[#4c19ae] text-white md:hidden mt-8' />
         </div>
     );
 };
