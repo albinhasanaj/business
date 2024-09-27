b82e078c5384c0c3d50cd325c8b00da32be9074c
ProgrammerFromSweden <albinhasanaj06@hotmail.com>
2024-09-27 22:57:57 +0200

    better navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,6 +12,9 @@ const Navbar = () => {
     const lastScrollY = useRef<number>(0);
     const ticking = useRef<boolean>(false);
 
+    const ignoreScroll = useRef<boolean>(false);
+    const ignoreScrollTimeout = useRef<NodeJS.Timeout | null>(null);
+
     const handleScroll = () => {
         const currentScrollY = window.scrollY;
 
@@ -26,7 +29,7 @@ const Navbar = () => {
     };
 
     const onScroll = () => {
-        if (!ticking.current) {
+        if (!ticking.current && !ignoreScroll.current) {
             window.requestAnimationFrame(handleScroll);
             ticking.current = true;
         }
@@ -42,62 +45,98 @@ const Navbar = () => {
         }
     }, []);
 
+    const handleNavLinkClick = () => {
+        ignoreScroll.current = true;
+        if (ignoreScrollTimeout.current) {
+            clearTimeout(ignoreScrollTimeout.current);
+        }
+        ignoreScrollTimeout.current = setTimeout(() => {
+            ignoreScroll.current = false;
+        }, 1000); // Adjust the duration as needed
+    };
+
     return (
         <nav
-            className={`fixed transition-all top-0 left-0 right-0 z-50 duration-200 ${show ? 'translate-y-0 bg-[rgba(0,0,0,0.3)] hover:bg-[rgba(0,0,0,0.5)]' : '-translate-y-full'
+            className={`fixed transition-all top-0 left-0 right-0 z-50 duration-200 ${show
+                    ? 'translate-y-0 bg-[rgba(0,0,0,0.3)] hover:bg-[rgba(0,0,0,0.5)]'
+                    : '-translate-y-full'
                 } flex w-full items-center text-white py-4 px-4 md:px-8 justify-around`}
         >
             <span className='text-4xl font-bold tracking-wide'>
-                <Link href='#home'>Tachyon</Link>
+                <Link href='#home' onClick={handleNavLinkClick}>Tachyon</Link>
             </span>
             <ul className='md:gap-4 lg:gap-8 hidden md:flex select-none'>
                 <li className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'>
-                    <Link href='#about'>About Us</Link>
+                    <Link href='#about' onClick={handleNavLinkClick}>About Us</Link>
                 </li>
                 <li className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'>
-                    <Link href='#projects'>Portfolio</Link>
+                    <Link href='#projects' onClick={handleNavLinkClick}>Portfolio</Link>
                 </li>
                 {/* <li className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'>
                     <Link href='#services'>Services</Link>
                 </li> */}
                 <li className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'>
-                    <Link href='#contact'>Contact</Link>
+                    <Link href='#contact' onClick={handleNavLinkClick}>Contact</Link>
                 </li>
             </ul>
-            <GrLanguage size={32} className='hidden md:flex hover:text-[#41BFF5] transition-colors cursor-pointer' />
+            <GrLanguage
+                size={32}
+                className='hidden md:flex hover:text-[#41BFF5] transition-colors cursor-pointer'
+            />
 
             {isOpen ? (
-                <RxCross2 size={32} className='md:hidden text-white cursor-pointer' onClick={() => setIsOpen(false)} />
+                <RxCross2
+                    size={32}
+                    className='md:hidden text-white cursor-pointer'
+                    onClick={() => {
+                        setIsOpen(false);
+                    }}
+                />
             ) : (
-                <CgMenuCheese size={32} className='md:hidden text-white cursor-pointer' onClick={() => setIsOpen(true)} />
+                <CgMenuCheese
+                    size={32}
+                    className='md:hidden text-white cursor-pointer'
+                    onClick={() => setIsOpen(true)}
+                />
             )}
 
             {isOpen && (
                 <div className='md:hidden absolute top-full right-4 w-[80%] max-w-sm bg-white rounded-lg shadow-lg p-6 z-40'>
                     <ul className='flex flex-col gap-6 text-black items-start select-none'>
                         <li
-                            onClick={() => setIsOpen(false)}
-                            className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'>
-                            <Link href='#about'>About Us</Link>
+                            onClick={() => {
+                                setIsOpen(false);
+                            }}
+                            className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'
+                        >
+                            <Link href='#about' onClick={handleNavLinkClick}>About Us</Link>
                         </li>
                         <li
-                            onClick={() => setIsOpen(false)}
-                            className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'>
-                            <Link href='#projects'>Portfolio</Link>
+                            onClick={() => {
+                                setIsOpen(false);
+                            }}
+                            className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'
+                        >
+                            <Link href='#projects' onClick={handleNavLinkClick}>Portfolio</Link>
                         </li>
                         {/* <li className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'>
                             <Link href='#services'>Services</Link>
                         </li> */}
                         <li
-                            onClick={() => setIsOpen(false)}
-                            className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'>
-                            <Link href='#contact'>Contact</Link>
+                            onClick={() => {
+                                setIsOpen(false);
+                            }}
+                            className='text-lg font-medium hover:text-[#41BFF5] transition-colors cursor-pointer'
+                        >
+                            <Link href='#contact' onClick={handleNavLinkClick}>Contact</Link>
                         </li>
-                        <GrLanguage size={32} className='mt-4 cursor-pointer text-black hover:text-[#41BFF5] transition-colors' />
+                        <GrLanguage
+                            size={32}
+                            className='mt-4 cursor-pointer text-black hover:text-[#41BFF5] transition-colors'
+                        />
                     </ul>
                 </div>
             )}
-
         </nav>
     );
 };
