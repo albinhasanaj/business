8da4c61cd319dd651ef7e17e3db692d0f321c6c4
Oliversoder06 <lexrudolf06@gmail.com>
2024-09-29 03:24:03 +0200

    bugfix: smileyspeak

--- app/[lang]/projects/smileyspeak/(main)/page.tsx
+++ app/[lang]/projects/smileyspeak/(main)/page.tsx
@@ -23,7 +23,7 @@ const SmileySpeak = () => {
                     <p className='text-gray-500 pt-[200px]'>For the full project, visit <span className='font-bold cursor-pointer'>[URL FOR THE PROJECT]</span></p>
                 </section>
             ) : (
-                <div className="h-screen bg-[#DDDDDD]">
+                <div className="min-h-screen bg-[#DDDDDD]">
                     <div className="flex flex-col items-center w-full">
                         <div className="w-full py-8">
                             <SmileySpeakNav />
