4404620fc51bc1b89bde427b6b9b9c26606eefc9
ProgrammerFromSweden <albinhasanaj06@hotmail.com>
2024-10-01 10:30:45 +0200

    implement smileyspeak

--- components/SmileySpeak/SmileySpeakNavLi.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SmileySpeakNavLi = () => {
-    return (
-        <div className='absolute top-full right-[-1.25rem] py-8 px-6 bg-[#C4C4C4] rounded-[15px] z-[99]'>Log Out</div>
-    )
-}
-
-export default SmileySpeakNavLi
